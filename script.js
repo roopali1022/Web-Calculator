38b4666cd886ec5046c7b1b2b10f1de207160c72
roopali1022 <roopali.lucky@gmail.com>
2020-09-25 21:17:15 -0400

    part-two-answer complete

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function sum()
+function calculate() {
+    value1 = parseInt(document.getElementById("value1").value);
+    value2 = parseInt(document.getElementById("value2").value);
+    document.getElementById("answer").innerHTML = operate(value1, value2);
+}
+function operate(value1,value2)
 {
-    var x = parseInt(document.getElementById("varx").value);
-    var y = parseInt(document.getElementById("vary").value);
-    var result =x+y;
-    document.getElementById("varresult").value = result;
-
+    var operator = document.getElementById("operator").value;
+    if (operator == '+')
+     {
+        return value1 + value2;
+    } 
+    else if (operator == '-') 
+    {
+        return value1 - value2;
+    } else if (operator == '*') 
+    {
+        return value1 * value2;
+    } else if (operator == '/')
+     {
+        return value1 / value2;
+    }
 }
